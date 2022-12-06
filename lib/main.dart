2125ef44864fd3588b5167963ad8a3d5e9247242
Hakan Kaplan <hknkaplan33@outlook.com>
2022-12-06 21:48:14 +0300

    main text yas eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'HAKAN KAPLAN',
+              'HAKAN KAPLAN 26',
             ),
             Text(
               '$_counter',
